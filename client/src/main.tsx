e9a7b4aebfc8e1c47a3b59216a8684eefe98ccb6
ZainulAzhan <zainulazhan2014@gmail.com>
2026-01-21 00:06:06 +0800

    End of section 7

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,13 +8,15 @@ import '@fontsource/roboto/700.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router';
+import { router } from './app/router/Router.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router}/>
       <TanStackDevtools
         plugins={[
           {
