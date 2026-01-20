6cc94cd419bf3be5c12d8d162186d4c3863fd5ff
ZainulAzhan <zainulazhan2014@gmail.com>
2026-01-20 20:28:09 +0800

    End of section 6

---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using Application.Activities.Queries;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+builder.Services.AddMediatR(cfg => 
+{
+    cfg.LicenseKey = builder.Configuration["MediatrLicenseKey"];
+    cfg.RegisterServicesFromAssemblyContaining<GetActivityList>();
+});
+builder.Services.AddAutoMapper(cfg => {
+    cfg.LicenseKey = builder.Configuration["MediatrLicenseKey"];
+    cfg.AddMaps([typeof(MappingProfiles)]);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+  .WithOrigins("https://localhost:3000","http://localhost:3000"));
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+  var context = services.GetRequiredService<AppDbContext>();
+  await context.Database.MigrateAsync();
+  await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "An error occurred during migration");
+}
+
+app.Run();
