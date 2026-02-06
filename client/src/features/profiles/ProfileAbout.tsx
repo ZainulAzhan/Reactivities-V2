9ddc588bc902e7daf19f838c67b3d8cc4d9f615d
ZainulAzhan <zainulazhan2014@gmail.com>
2026-02-06 18:49:05 +0800

    End of section 18

--- client/src/features/profiles/ProfileAbout.tsx
+++ client/src/features/profiles/ProfileAbout.tsx
@@ -1,21 +1,30 @@
 import { useParams } from "react-router"
 import { useProfile } from "../../lib/hooks/useProfile";
 import { Box, Button, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import ProfileEdit from "./ProfileEdit";
 
 export default function ProfileAbout() {
   const {id} = useParams();
   const { profile } = useProfile(id);
+  const [editMode, setEditMode] = useState(false);
+
   return (
     <Box>
       <Box display='flex' justifyContent='space-between'>
         <Typography variant="h5">About {profile?.displayName}</Typography>
-        <Button>Edit Profile</Button>
+        <Button onClick={() => setEditMode(!editMode)}>
+          Edit Profile
+        </Button>
       </Box>
       <Divider sx={{my: 2}}/>
       <Box>
         <Typography variant="body1" sx={{whiteSpace: 'pre-wrap'}}>
           {profile?.bio || 'No description added yet'}
         </Typography>
+        {editMode && (
+          <ProfileEdit setEditMode={setEditMode} />
+        )}
       </Box>
     </Box>
   );
