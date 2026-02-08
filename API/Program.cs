e45c29fdca58db41f503673ed81b8bae5bd521fe
ZainulAzhan <zainulazhan2014@gmail.com>
2026-02-08 11:50:42 +0800

    End of section 19

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using API.SignalR;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
@@ -28,6 +29,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddSignalR();
 builder.Services.AddMediatR(cfg => 
 {
     cfg.LicenseKey = builder.Configuration["MediatrLicenseKey"];
@@ -73,6 +75,7 @@ app.UseAuthentication();
 app.UseAuthorization(); 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<User>();
+app.MapHub<CommentHub>("/comments");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
